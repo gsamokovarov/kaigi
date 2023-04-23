3fae8d4e5b364fb80aec846a3cbf722afc8909ab
Genadi Samokovarov <gsamokovarov@gmail.com>
2023-04-23 16:20:18 +0300

    Put a newline before multiline examples

--- app/models/parsable_ruby_slim_engine.rb
+++ app/models/parsable_ruby_slim_engine.rb
@@ -2,7 +2,7 @@ class ParsableRubySlimEngine < Slim::Embedded::Engine
   def on_slim_embedded(_engine, body, _attrs)
     [:static, <<~HTML]
       <pre class="js-parse-ruby">
-        <code contenteditable class="ruby">#{collect_text(body)}</code>
+        <code contenteditable class="ruby">#{"\n" + collect_text(body)}</code>
       </pre>
     HTML
   end
