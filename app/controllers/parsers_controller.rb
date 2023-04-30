d96a9f737584ac13c0f8d770160580c2d284e8d4
Genadi Samokovarov <gsamokovarov@gmail.com>
2023-04-30 16:49:35 +0300

    Follow current ruby version when parsing

--- app/controllers/parsers_controller.rb
+++ app/controllers/parsers_controller.rb
@@ -1,10 +1,10 @@
-require 'parser/ruby32'
+require 'parser/current'
 
 class ParsersController < ApplicationController
   skip_forgery_protection
 
   def create
-    ast = Parser::Ruby32.parse params[:code]
+    ast = Parser::CurrentRuby.parse params[:code]
     code = ast.inspect
 
     render json: { code: }
